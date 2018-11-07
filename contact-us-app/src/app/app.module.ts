e0e95ac82dd6576006e22eed5468ed396232b329
Dare2Code <Dare2Code@users.noreply.github.com>
2018-11-06 20:28:37 -0500

    Component - "Conatct us" Created
    Created Contact us Component.

--- contact-us-app/src/app/app.module.ts
+++ contact-us-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
